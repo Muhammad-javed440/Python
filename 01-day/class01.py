a690fb86631da740c6bb9076018acb4ce5ed57cb
Muhammad Javed <141915304+Muhammad-javed440@users.noreply.github.com>
2025-01-04 20:41:57 +0500

    Add files via upload

---
+++ 01-day/class01.py
@@ -0,0 +1,11 @@
+print('Hello World')
+
+print(5+6)
+
+# PEMDAS(pranthesis exponent multiply  divide add subtract)
+
+print(3**2/2*3/3+6-4/(2*8))
+
+
+## vpn user name   javedstore440
+## vpn password   Javed@store2570
---
+++ 01-day/comment.py
@@ -0,0 +1,9 @@
+# Subject of this python script:
+# Authored by: Muhammad Javed
+# Where to contact: github profile link  "or"  email address
+# Date of creation: 2023-02-20
+#  Last modified: 2023-02-20
+# Description: This script is used to create a simple calculator that can perform addition, subtraction, multiplication
+# and division operations.
+
+
