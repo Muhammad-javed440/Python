cb7ce160ea5d73a6ed41692ae65df4bbdbf37cdf
Muhammad Javed <141915304+Muhammad-javed440@users.noreply.github.com>
2025-01-04 20:50:24 +0500

    Add files via upload

---
+++ 04-day/import_libraries.py
@@ -0,0 +1,8 @@
+# if you want to print the value of pi
+
+import math
+print('The value of pi is :', math.pi)
+
+import statistics
+x = [150,250,350,450]
+print('Mean of the given data is :',statistics.mean(x))
---
+++ 04-day/variable_rules.py
@@ -0,0 +1,9 @@
+# Rules to assign a variable:
+
+# 1- The variable should contain letters, numbers or underscores
+# 2- Do not start with numbers
+# 3- The variables are not allowed spaces between them 
+# 4- Do not use keywords as a variable name
+# 5- The variable should be unique
+# 6- Short and descriptive 
+# 7- Case senstivity
